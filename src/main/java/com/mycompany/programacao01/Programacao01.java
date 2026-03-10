b143dc544b4c28954460ab705f6875c54db51407
aluno <aluno@lab01-23>
2026-03-10 14:12:54 -0300

    testes

--- src/main/java/com/mycompany/programacao01/Programacao01.java
+++ src/main/java/com/mycompany/programacao01/Programacao01.java
@@ -11,6 +11,16 @@ package com.mycompany.programacao01;
 public class Programacao01 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Estudante camada = new Estudante();
+        Estudante leonardo = new Estudante();
+        Estudante jefferson = new Estudante();
+        
+        camada.setNome("Marcos");
+        leonardo.setNome("leonardo");
+        jefferson.setNome("jefferson");
+        
+        System.out.println(camada.getNome());
+        System.out.println(leonardo.getNome());
+        System.out.println(jefferson.getNome());
     }
 }
