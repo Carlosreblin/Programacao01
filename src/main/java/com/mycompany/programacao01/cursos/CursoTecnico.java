d3906845954177603b5b11228c26e496abe22f89
Carlos Eduardo Reblin <carlosereblin@gmail.com>
2026-04-07 13:54:35 -0300

    Implement realizarMatricula method in CursoTecnico

--- src/main/java/com/mycompany/programacao01/cursos/CursoTecnico.java
+++ src/main/java/com/mycompany/programacao01/cursos/CursoTecnico.java
@@ -18,6 +18,11 @@ public class CursoTecnico extends Curso{
     }
 
     @Override
+    public void realizarMatricula(){
+        
+    }
+
+    @Override
     public List<Diciplina> obterDiciplinas() {
         return List.of();
     }
